b8e223dc528cc8d9750460646fb4bf70ca7579ec
yun <2212032@exchange.prince.com.tw>
2023-01-31 11:03:12 +0800

    Revert "Rename file"This reverts commit fafb0121f9e47b1bdc4b5d91a01871a13d210be5.# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch main# Your branch is ahead of 'origin/main' by 1 commit.#   (use "git push" to publish your local commits)## Changes to be committed:#	renamed:    controllers/taskController.js -> controllers/task.js#	modified:   package-lock.json#	modified:   routes/task.js#

--- routes/task.js
+++ routes/task.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAllTasks, createTasks, updateTasks, getTask, deleteTask } = require('../controllers/taskController');
+const { getAllTasks, createTasks, updateTasks, getTask, deleteTask } = require('../controllers/task');
 
 router.route('/').get(getAllTasks).post(createTasks);
 router.route('/:id').get(getTask).patch(updateTasks).delete(deleteTask);
