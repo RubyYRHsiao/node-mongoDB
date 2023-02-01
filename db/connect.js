d460f3fe768b99f506780d2f54299275ea739a3b
Yun <momo1425360@gmail.com>
2023-02-01 22:23:38 +0800

    Refactor db connection

--- db/connect.js
+++ db/connect.js
@@ -4,10 +4,12 @@ mongoose.set('strictQuery', true);
 const connectionString =
     'mongodb+srv://yun:1234@nodetest1.y9iotuw.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority';
 
-mongoose
-    .connect(connectionString, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log('CONNECTED TO THE DB...'))
-    .catch((err) => console.log(err));
+const connectDB = (url) => {
+    return mongoose
+        .connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+}
+
+module.exports = connectDB;
